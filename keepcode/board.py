ad0ed231c6adc01ad577ebaaf8018480f5b69222
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2019-01-25 07:41:20 +0100

    disabling update is someone else is editing

--- keepcode/board.py
+++ keepcode/board.py
@@ -62,7 +62,7 @@ def create():
 def get_post(id, check_author=False):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id,'
-        ' last_updated, programming_language, username'
+        ' last_updated, programming_language, username, being_updated'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
