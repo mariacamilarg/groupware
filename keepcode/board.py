82697c991fd45549d1f9eaa72fc34f362e3a07ca
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-01-27 14:19:43 +0100

    To return value being_updated to index view

--- keepcode/board.py
+++ keepcode/board.py
@@ -15,7 +15,7 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id,'
-        ' last_updated, programming_language, username'
+        ' last_updated, programming_language, username, being_updated'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
