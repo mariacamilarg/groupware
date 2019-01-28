fc423021eef5dd55066d3b4c5b95c33074c8efab
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-01-28 23:12:43 +0100

    Set path for db

--- keepcode/__init__.py
+++ keepcode/__init__.py
@@ -10,9 +10,13 @@ import board
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'keepcode.sqlite'),
+    # )
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'keepcode.sqlite'),
+        DATABASE='../instance/keepcode.sqlite',
     )
 
     if test_config is None:
@@ -41,3 +45,4 @@ def create_app(test_config=None):
     return app
 
 app = create_app()
+#app.run()
